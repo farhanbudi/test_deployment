298fd48a5ab31f7b2aafceab57fbaa19327b91ef
farhanbudi <farhannurrizky@gmail.com>
2021-09-25 11:34:32 +0700

    add new text

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>Add new text to check deployment</p>
         <a
           className="App-link"
           href="https://reactjs.org"
